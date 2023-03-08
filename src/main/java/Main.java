7a536ebdcf14fec86dce9dce7f58ff3e9afe3a3f
stellakeany <stella.keany@ucdconnect.ie>
2023-03-08 22:42:49 +0000

    Version String Split Implemented
    Splits version strings with "." as delimiter to allow easy comparison

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        VersionCompare.compareVersion("1.2.3", "0.1.2");
     }
 }
--- src/main/java/VersionCompare.java
+++ src/main/java/VersionCompare.java
@@ -1,2 +1,26 @@
-public class VersionCompare {
+import java.util.Arrays;
+
+public final class VersionCompare {
+
+    /**
+     * Utility method which compares two versions
+     * @param version1 First version in String form
+     * @param version2 Second version in String form
+     * @return If version1 > version2 returns 1, <br>
+     * If version1 < version2 returns -1, <br>
+     * If version1 = version2 returns, 0
+     */
+    public static int compareVersion(String version1, String version2){
+
+        // Split each version string into string array using "." as delimiter
+        // i.e. 0.0.1.3 becomes ["0","0","1","3"]
+        String[] v1 = version1.split("\\.");
+        String[] v2 = version2.split("\\.");
+
+        System.out.println(Arrays.toString(v1));
+        System.out.println(Arrays.toString(v2));
+
+        return 1;
+    }
+    private VersionCompare(){}
 }
