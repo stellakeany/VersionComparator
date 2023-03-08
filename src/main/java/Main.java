a18f0a63d50434f4b3869c9192cbb706b45feebe
stellakeany <stella.keany@ucdconnect.ie>
2023-03-08 23:01:34 +0000

    compareVersion Fully Implemented
    Requires testing.
    Adheres to assumptions that version strings are non-empty and contain only digits and the 'dot' character.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        VersionCompare.compareVersion("1.2.3", "0.1.2");
+        System.out.println(VersionCompare.compareVersion("1.0.0", "1.0"));
     }
 }
--- src/main/java/VersionCompare.java
+++ src/main/java/VersionCompare.java
@@ -3,7 +3,10 @@ import java.util.Arrays;
 public final class VersionCompare {
 
     /**
-     * Utility method which compares two versions
+     * <h1>Version Compare<h1/>
+     * <h2>Utility method which compares two versions<h2/>
+     * <p>Operates by splitting each version into an array of strings using "." as the delimiter. The arrays are then
+     * compared index-wise until two strings are not equal or until the end of both arrays has been reached.<p/>
      * @param version1 First version in String form
      * @param version2 Second version in String form
      * @return If version1 > version2 returns 1, <br>
@@ -12,15 +15,21 @@ public final class VersionCompare {
      */
     public static int compareVersion(String version1, String version2){
 
-        // Split each version string into string array using "." as delimiter
         // i.e. 0.0.1.3 becomes ["0","0","1","3"]
         String[] v1 = version1.split("\\.");
         String[] v2 = version2.split("\\.");
 
-        System.out.println(Arrays.toString(v1));
-        System.out.println(Arrays.toString(v2));
+        int longestString = Math.max(v1.length, v2.length);
 
-        return 1;
+        for (int i = 0; i < longestString; i++) {
+            int num1 = i < v1.length ? Integer.parseInt(v1[i]) : 0; // if end of shorter string reached return 0 since 1.0 == 1.0.0 etc
+            int num2 = i < v2.length ? Integer.parseInt(v2[i]) : 0;
+
+            if (num1 > num2){return 1;}
+            if (num2 > num1){return -1;}
+        }
+
+        return 0;
     }
     private VersionCompare(){}
 }
